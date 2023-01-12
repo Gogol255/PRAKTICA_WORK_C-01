96b715ce57223b81d4b4f758c620ff272a43fafc
amazon_2004@mail.ru <amazon_2004@mail.ru>
2023-01-12 13:43:30 +0300

    приступила к выполнению задач 2 практической работы.решила 1 задачу через конвертацию и обращения к символу, а 2- остаток от деления

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string str = a.ToString();
+
+if(str.Length >2)
+{
+  Console.WriteLine(str[1]);
+}
